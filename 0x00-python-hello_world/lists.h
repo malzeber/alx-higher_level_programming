ea56bb9a828cc646adf2639423fca0c021809cba
malzeber <m.alzeber97@gmail.com>
2023-01-02 13:44:42 -0800

    update

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -7,7 +7,6 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- * 
  * Description: singly linked list node structure
  * for Holberton project
  */
@@ -21,5 +20,4 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
-
 #endif /* LISTS_H */
