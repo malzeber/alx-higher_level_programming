20cc76b737bcb636fe9dd1bbbd45ce2934183bd2
malzeber <m.alzeber97@gmail.com>
2023-01-15 22:00:41 +0200

    updata

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     elif count == 1:
         print("1 argument:")
     else:
-        print(f"{cont} arguments:")
+        print(f"{count} arguments:")
     for i in range(count):
         print(f"{i + 1}: {sys.argv[i + 1]}")
